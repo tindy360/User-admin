6b736161d60e5343de6b204bdd14206ba427d7b5
tindy360 <tindy360>
2017-01-24 19:39:43 -0500

    update

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const modles = mongoose.Schema;
-const expressValidator = require('express-validator');
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise,
+//const {PORT, DATABASE_URL} = require('module');
 
 
+mongoose.connect('mongodb://localhost/userdb/users')
 
 app.use(express.static(__dirname + '/public'));
-//validation middleware
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
 
 app.get('/', (req, res)=> {
     res.send('index');
@@ -34,25 +19,19 @@ app.get('/users', (req, res)=>{
 });
 
 app.post('/users/add', (err, req, res)=>{
-    req.checkBody('firstName', 'Please enter a first name.').notEmpty();
-    req.checkBody('lastName', 'Please enter a last name.').notEmpty();
-    req.checkBody('email', 'Please enter an email.').notEmpty();
-    /*req.check({
-      'email': {
-        in: body
-        notEmpty: true,
-        isEmail: {}
-      }
-  })*/
-   const errors = req.validationErrors();
+//  if error, {
+  //  throw error
 
+  //else {
     const newUser = {
-      firstName: req.body.firstName
-      lastName: req.body.lastName
+      userName: req.body.userName
+      password: req.body.password
       email: req.body.email
-    }
+    //}
     console.log('user added')
   }
+  }
+
 
 });
 
