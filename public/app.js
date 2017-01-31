1d6bbfa8a3e00b0e3e08b4da8675c702e81adda5
tindy360 <tindy360>
2017-01-31 17:26:27 -0500

    UI updates logic refactored a bit for reusablitiy

--- public/app.js
+++ public/app.js
@@ -24,26 +24,25 @@ $(document).ready(function() {
             type: "POST",
             data: formData,
             success: function(data, textStatus, jqXHR) {},
-            error: function(jqXHR, textStatus, errorThrown) {
+            error: function(jqXHR, textStatus, errorThrown) {}
 
-            }
         });
         this.reset();
-    });
+        $('#formModal').modal('hide');
+    }); //submit closes
     //get request for rendering users to DOM
-    $('#getUsers').click(function() {
-        console.log('users requested'),
-        $.getJSON("/users", function(users){
-          renderUsers(users);
-        })
-    });
+    function retreiveUsers() {
+        $.getJSON("/users", function(users) {
+            renderUsers(users);
+        });
+    };
 
-function renderUsers(users){
-  console.log(users);
- $.each(users, function(i, value) {
-   console.log(value.email),
-   $('ul').append('<li>' + value.userName + '|' + '&nbsp' + value.email + '</li>')
- });
-}
+    function renderUsers(users) {
+        console.log(users);
+        $.each(users, function(i, value) {
 
+            $('#userTable').append('<tr><td>' + value.userName + '</td><td>' + value.email + '</td><td>' + '<button type="button" class="btn btn-danger btn-sm">Delete User</button>' + '</td></tr>')
+        });
+    }
+    retreiveUsers();
 }); //close of doc ready function
